4f0851e28851f2465c4058a5c189e47d5d073118
Jiun-yi Tsai <45142992+byjiyi@users.noreply.github.com>
2021-08-03 14:52:47 +0800

    Delete backend directory

--- backend/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { startFastify } from './server'
-
-// Start your server
-const server = startFastify(8888)
-
-export { server }
--- backend/src/types/maintain_record.ts
+++
@@ -1,8 +0,0 @@
-type IRecord = {
-    readonly _id?: string
-    formid:string
-    maintain_description: string
-    readonly createdAt?: Date
-  }
-  
-  export { IRecord }
