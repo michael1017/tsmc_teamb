01730fb6c2d08bf0eb5245aaf27975f74b12d7b5
yubintw <yubin.cs05g@nctu.edu.tw>
2021-08-07 20:20:32 +0800

    delete build file

--- bot/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.server = void 0;
-var server_1 = require("./server");
-// Start your server
-var server = server_1.startFastify(6666);
-exports.server = server;
