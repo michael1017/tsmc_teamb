2b5243ae410b775d906118fdedefd05c4fdb896a
Jiun-yi Tsai <45142992+byjiyi@users.noreply.github.com>
2021-08-08 09:51:49 +0800

    Delete backend/out directory

--- backend/out/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.server = void 0;
-var server_1 = require("./server");
-// Start your server
-var server = server_1.startFastify(8888);
-exports.server = server;
