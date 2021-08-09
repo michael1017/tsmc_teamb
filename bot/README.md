f54e577543fb6475770248b61967d762a9f36d5b
yubintw <yubin.cs05g@nctu.edu.tw>
2021-08-09 23:13:01 +0800

    add Dockerfile for bot

--- bot/README.md
+++ bot/README.md
@@ -1,3 +1,15 @@
 # Bot Server Example
 
 using fastify webhook server
+
+## build bot image
+
+```
+docker build -t b-bot .
+```
+
+## run bot container
+
+```
+docker run -d -p 6666:6666 --name b-bot b-bot
+```
--- bot/src/index.ts
+++ bot/src/index.ts
@@ -1,6 +1,7 @@
 import { startFastify } from './server'
 
 // Start your server
-const server = startFastify(6666)
+const port = process.env.FASTIFY_PORT || 6666
+const server = startFastify(Number(port))
 
 export { server }
